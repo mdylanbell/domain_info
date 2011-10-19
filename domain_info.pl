e347457ca4e6fe24884094411a92513543a1337f
Matthew Bell <m.dylan.bell@gmail.com>
2011-10-19 12:52:57 -0700

    Filter out comment lines in longform WHOIS parsing

--- domain_info.pl
+++ domain_info.pl
@@ -219,7 +219,7 @@ sub get_orgname {
     my $result = '';
 
     foreach (@results) {
-        next if (/^[\s#]|^$|Internet Numbers/);
+        next if (/^[\s#]|^$|Internet Numbers/ || /^Comment: /);
         $result = $_;
 
         # Some WHOIS servers use longform
@@ -236,7 +236,7 @@ sub get_orgname {
         #   Example of our $result at this point (hopefully):
         #   Google Inc. GOOGLE (NET-74-125-0-0-1) 74.125.0.0 - 74.125.255.255
 
-        if ( $result =~ /(.*?)(\s*[\w\d\._\-]*\s*)?\(/ ) {
+        if ( !$org && $result =~ /(.*?)(\s*[\w\d\._\-]*\s*)?\(/ ) {
             $org = $1;
             last;
         }
